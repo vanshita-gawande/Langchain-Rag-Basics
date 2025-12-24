71ae0fef06e3f459dec663a21395333da1be1459
Vanshita Gawande <142472214+vanshita-gawande@users.noreply.github.com>
2025-12-24 16:52:29 +0530

    ChatModels

--- chatmodels/chatmodel_gemini.py
+++ chatmodels/chatmodel_gemini.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
-load_dotenv() #we get our api key here from env
+load_dotenv() #we get our api key here from env file
 
 model = ChatGoogleGenerativeAI(model = "gemini")
 result = model.invoke("pass here question")
